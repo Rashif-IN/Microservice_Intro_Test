0a8fd879325b6684a96b8dd4eff7d4993b49d889
rashif_ilmi <rashif.ilmi@gigaming.io>
2020-03-13 16:57:45 +0700

    added composer

---
+++ Merchant_Handler/Merchant_Handler/Migrations/20200313091952_migrationmerchant.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Merchant_Handler.Migrations
+{
+    public partial class migrationmerchant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
